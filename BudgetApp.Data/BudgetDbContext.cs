88b27b9a0bb774542dd6270898fa625f088c9603
Michael Carey <careyamichael2002@gmail.com>
2026-09-08 20:05:46 -0600

    BUDGET-8: Created data project with transaction table

---
+++ BudgetApp.Data/BudgetDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BudgetApp.Data.Entites;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetApp.Data
+{
+    public class BudgetDbContext(DbContextOptions<BudgetDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<Transaction> Transactions{ get; set; }
+    }
+}
---
+++ BudgetApp.Data/Migrations/20260909020443_InitTransactionTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitTransactionTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    AccountID = table.Column<int>(type: "int", nullable: false),
+                    CategoryID = table.Column<int>(type: "int", nullable: false),
+                    ReceiptID = table.Column<int>(type: "int", nullable: false),
+                    Merchant = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    AmountCents = table.Column<int>(type: "int", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
